43736dbacc55ded10859af19a5490d65d6c44ba0
swayamprabhu2005 <swayamkiranprabhu2005@gmail.com>
2026-02-15 16:59:58 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/waterMonitoring", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect("mongodb://127.0.0.1:27017/waterMonitoring");
 
         console.log("✅ MongoDB Connected Successfully");
     } catch (error) {
