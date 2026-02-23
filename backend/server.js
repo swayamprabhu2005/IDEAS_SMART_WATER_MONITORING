0e7badbe9e7fd0c72ead31ae0ff26d39dda77f65
swayamprabhu2005 <swayamkiranprabhu2005@gmail.com>
2026-02-23 22:39:34 +0530

    chatbot and server.js changes

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,6 @@ app.use("/api", dataRoutes);
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.listen(5000, "0.0.0.0", () => {
-    console.log("🚀 Server running on http://0.0.0.0:5000");
+    console.log("Server running on http://localhost:5000");
 });
 
